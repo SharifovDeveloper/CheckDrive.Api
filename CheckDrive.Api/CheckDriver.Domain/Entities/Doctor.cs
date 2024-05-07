9583183d97500e2f6676ec67ba9f582e82ad66c6
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-05-07 16:24:02 +0500

    Create-Doctor-Entitiy

---
+++ CheckDrive.Api/CheckDriver.Domain/Entities/Doctor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckDriver.Domain.Entities
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public Account? Account { get; set; }
+
+        public ICollection<DoctorReview>? DoctorReviews { get; set; }
+    }
+}
