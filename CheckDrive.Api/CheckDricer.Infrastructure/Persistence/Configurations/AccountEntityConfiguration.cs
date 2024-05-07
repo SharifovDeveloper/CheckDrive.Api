05bea2a85a08e861534255a29d52aced7c9d67f8
diyorzz <128776299+diyorzz@users.noreply.github.com>
2024-05-07 18:06:07 +0500

    Add AccountEntityConfiguration

---
+++ CheckDrive.Api/CheckDricer.Infrastructure/Persistence/Configurations/AccountEntityConfiguration.cs
@@ -0,0 +1,63 @@
+﻿using CheckDriver.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CheckDricer.Infrastructure.Persistence.Configurations
+{
+    internal class AccountEntityConfiguration : IEntityTypeConfiguration<Account>
+    {
+        public void Configure(EntityTypeBuilder<Account> builder)
+        {
+            builder.ToTable(nameof(Account));
+            builder.HasKey(x => x.Id);
+
+            builder.Property(a => a.Login)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.Property(a => a.Password)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.Property(a => a.FirstName)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.Property(a => a.LastName)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.Property(a => a.PhoneNumber)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.HasOne(a => a.Role)
+                .WithMany(r => r.Accounts)
+                .HasForeignKey(a => a.RoleId);
+
+            builder.HasMany(a => a.Dispatchers)
+                .WithOne(x => x.Account)
+                .HasForeignKey(x => x.AccountId);
+
+            builder.HasMany(a => a.Operators)
+                .WithOne(x => x.Account)
+                .HasForeignKey(x => x.AccountId);
+
+            builder.HasMany(a => a.Mechanics)
+                .WithOne(x => x.Account)
+                .HasForeignKey(x => x.AccountId);
+
+            builder.HasMany(a => a.Drivers)
+                .WithOne(x => x.Account)
+                .HasForeignKey(x => x.AccountId);
+
+            builder.HasMany(a => a.Doctors)
+                .WithOne(x => x.Account)
+                .HasForeignKey(x => x.AccountId);
+
+            builder.HasMany(a => a.Technicians)
+                .WithOne(x => x.Account)
+                .HasForeignKey(x => x.AccountId);
+        }
+    }
+}
