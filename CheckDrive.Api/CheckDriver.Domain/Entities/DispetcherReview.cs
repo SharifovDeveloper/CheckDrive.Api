1a04548cdd1b4c3aadec5816f338cc753ff6122f
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-05-07 16:23:47 +0500

    Create-DispatcherReview-Entity

---
+++ CheckDrive.Api/CheckDriver.Domain/Entities/DispetcherReview.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckDriver.Domain.Entities
+{
+    public class DispetcherReview
+    {
+        public int Id { get; set; }
+        public double FuelSpended {  get; set; }
+        public double DistanceCovered { get; set; }
+        public DateTime Date { get; set; }
+
+        public int DispatcherId { get; set; }
+        public Dispatcher? Dispatcher { get; set; }
+        public int OperatorId { get; set; }
+        public Operator? Operator { get; set; }
+        public int MechanicId { get; set; }
+        public Mechanic? Mechanic { get; set; }
+        public int DriverId { get; set; }
+        public Driver? Driver { get; set; }
+    }
+}
