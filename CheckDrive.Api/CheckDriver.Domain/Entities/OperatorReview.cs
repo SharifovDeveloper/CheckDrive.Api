5f5d9f7b3d0ccd78cf1679a3d0bd855177458193
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-05-07 16:25:31 +0500

    Create-OperatorReview-Entity

---
+++ CheckDrive.Api/CheckDriver.Domain/Entities/OperatorReview.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckDriver.Domain.Entities
+{
+    public class OperatorReview
+    {
+        public double OilAmount { get; set; }
+        public string? Comments { get; set; }
+        public Status Status { get; set; }
+        public DateTime Date { get; set; }
+
+        public int OperatorId { get; set; }
+        public Operator? Operator { get; set; }
+        public int DriverId { get; set; }
+        public Driver? Driver { get; set; }
+    }
+}
