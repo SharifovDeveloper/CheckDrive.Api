73883188e8bc2fd4df2888b704062bb454981ba2
diyorzz <128776299+diyorzz@users.noreply.github.com>
2024-05-07 18:06:34 +0500

    Add DispatcherEntityConfiguration

---
+++ CheckDrive.Api/CheckDricer.Infrastructure/Persistence/Configurations/DispatcherEntityConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using CheckDriver.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CheckDricer.Infrastructure.Persistence.Configurations
+{
+    internal class DispatcherEntityConfiguration : IEntityTypeConfiguration<Dispatcher>
+    {
+        public void Configure(EntityTypeBuilder<Dispatcher> builder)
+        {
+            builder.ToTable(nameof(Dispatcher));
+            builder.HasKey(t => t.Id);
+
+
+            builder.HasOne(x => x.Account)
+                .WithMany(a => a.Dispatchers)
+                .HasForeignKey(x => x.AccountId);
+
+            builder.HasMany(a => a.DispetcherReviews)
+                .WithOne(x => x.Dispatcher)
+                .HasForeignKey(x => x.DispatcherId);
+        }
+    }
+}
