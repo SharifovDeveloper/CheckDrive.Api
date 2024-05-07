35d78d001f580d59799b32c4f109f5dde4ccff18
diyorzz <128776299+diyorzz@users.noreply.github.com>
2024-05-07 18:06:22 +0500

    Add CarEntityConfiguration

---
+++ CheckDrive.Api/CheckDricer.Infrastructure/Persistence/Configurations/CarEntityConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using CheckDriver.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CheckDricer.Infrastructure.Persistence.Configurations
+{
+    internal class CarEntityConfiguration : IEntityTypeConfiguration<Car>
+    {
+        public void Configure(EntityTypeBuilder<Car> builder)
+        {
+            builder.ToTable(nameof(Car));
+            builder.HasKey(c => c.Id);
+
+            builder.Property(x => x.Model)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.Property(x => x.Color)
+                .HasMaxLength(255)
+                .IsRequired();
+
+            builder.Property(x => x.Number)
+                .HasMaxLength(8)
+                .IsRequired();
+
+            builder.Property(x => x.ManufacturedYear)
+                .HasColumnType("integer")
+                .IsRequired();
+
+            builder.Property(x => x.FuelConsumptionFor100Km)
+                .HasColumnType("double")
+                .IsRequired();
+
+            builder.Property(x => x.FuelTankCapacity)
+                .HasColumnType("double")
+                .IsRequired();
+
+            builder.HasMany(c => c.MechanicHandovers)
+                .WithOne(m => m.Car)
+                .HasForeignKey(m => m.CarId);
+        }
+    }
+}
