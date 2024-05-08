0d62f3f60e6eb05b0c0f4579858b208954dd7810
diyorzz <128776299+diyorzz@users.noreply.github.com>
2024-05-08 15:49:31 +0500

    improve entities

--- CheckDrive.Api/CheckDriver.Domain/Entities/DoctorReview.cs
+++ CheckDrive.Api/CheckDriver.Domain/Entities/DoctorReview.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public bool IsHealthy { get; set; }
-        public string Comments { get; set; }
+        public string? Comments { get; set; }
         public DateTime Date { get; set; }
 
         public int DriverId { get; set; }
