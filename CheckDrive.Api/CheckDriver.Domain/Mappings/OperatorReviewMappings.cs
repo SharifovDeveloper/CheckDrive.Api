95d6f54911c34b70c6272694c88fca57f5996fc8
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-05-09 17:55:47 +0500

    Create-OperatorReviewMappings

---
+++ CheckDrive.Api/CheckDriver.Domain/Mappings/OperatorReviewMappings.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CheckDrive.Domain.DTOs.OperatorReview;
+using CheckDriver.Domain.Entities;
+
+namespace CheckDrive.Domain.Mappings
+{
+    public class OperatorReviewMappings : Profile
+    {
+        public OperatorReviewMappings() 
+        {
+            CreateMap<OperatorReviewDto, OperatorReview>();
+            CreateMap<OperatorReview, OperatorReviewDto>();
+            CreateMap<OperatorReviewForCreateDto, OperatorReview>();
+            CreateMap<OperatorReviewForUpdateDto, OperatorReview>();
+        }
+    }
+}
