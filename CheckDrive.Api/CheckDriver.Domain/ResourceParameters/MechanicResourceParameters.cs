17c8df3c2f942b2a0850842a5b91565263fcb9f1
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-05-09 13:13:06 +0500

    MechanicResourceParameters

---
+++ CheckDrive.Api/CheckDriver.Domain/ResourceParameters/MechanicResourceParameters.cs
@@ -0,0 +1,8 @@
+﻿namespace CheckDrive.Domain.ResourceParameters
+{
+    public class MechanicResourceParameters : ResourceParametersBase
+    {
+        public int? AccountId { get; set; }
+        public override string OrderBy { get; set; } = "id";
+    }
+}
