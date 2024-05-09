208f43319d73bb1f23f6499cdd689261e1845185
diyorzz <128776299+diyorzz@users.noreply.github.com>
2024-05-09 16:21:21 +0500

    Add Pagination and use

---
+++ CheckDrive.Api/CheckDriver.Domain/Pagniation/PaginationExtension.cs
@@ -0,0 +1,21 @@
+﻿using CheckDriver.Domain.Common;
+using Microsoft.EntityFrameworkCore;
+
+namespace CheckDrive.Domain.Pagniation
+{
+    public static class PaginationExtension
+    {
+        public static async Task<PaginatedList<T>> ToPaginatedListAsync<T>(
+            this IQueryable<T> source,
+            int pageSize,
+            int pageNumber) where T : EntityBase
+        {
+            var count = await source.CountAsync();
+            var items = await source.Skip((pageNumber - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            return new PaginatedList<T>(items, count, pageNumber, pageSize);
+        }
+    }
+}
