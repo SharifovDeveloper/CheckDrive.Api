06146725659199ea0de73ab3c285b02b6196a491
diyorzz <128776299+diyorzz@users.noreply.github.com>
2024-05-09 13:47:38 +0500

    AccountDTOs

---
+++ CheckDrive.Api/CheckDriver.Domain/DTOs/Account/AccountForUpdateDto.cs
@@ -0,0 +1,16 @@
+﻿using CheckDrive.Domain.DTOs.Role;
+
+namespace CheckDrive.Domain.DTOs.Account
+{
+    public class AccountForUpdateDto
+    {
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string PhoneNumber { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime Bithdate { get; set; }
+
+        public RoleDto RoleDto { get; set; }
+    }
+}
