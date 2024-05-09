a19252d7cd139c912f8fecb5d24bedac90fc6ec0
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-05-09 13:14:12 +0500

    CarResourceParameters

---
+++ CheckDrive.Api/CheckDriver.Domain/ResourceParameters/CarResourceParameters.cs
@@ -0,0 +1,16 @@
+﻿namespace CheckDrive.Domain.ResourceParameters
+{
+    public class CarResourceParameters : ResourceParametersBase
+    {
+        public double? MeduimFuelConsumption { get; set; }
+        public double? MeduimFuelConsumptionLessThan { get; set; }
+        public double? MeduimFuelConsumptionGreaterThan { get; set; }
+        public double? FuelTankCapacity { get; set; }
+        public double? FuelTankCapacityLessThan { get; set; }
+        public double? FuelTankCapacityThan { get; set; }
+        public int? ManufacturedYear { get; set; }
+        public int? ManufacturedYearLessThan { get; set; }
+        public int? ManufacturedYearGreaterThan { get; set; }
+        public override string OrderBy { get; set; } = "model";
+    }
+}
