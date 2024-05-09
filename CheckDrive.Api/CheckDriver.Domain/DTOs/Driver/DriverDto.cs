e4d33c6228a6ca4a3e2680430d0d036bdef48b0e
diyorzz <128776299+diyorzz@users.noreply.github.com>
2024-05-09 13:49:15 +0500

    DriverDTOs

---
+++ CheckDrive.Api/CheckDriver.Domain/DTOs/Driver/DriverDto.cs
@@ -0,0 +1,10 @@
+﻿using CheckDrive.Domain.DTOs.Account;
+
+namespace CheckDrive.Domain.DTOs.Driver
+{
+    public class DriverDto
+    {
+        public int Id { get; set; }
+        public AccountDto AccountDto { get; set; }
+    }
+}
---
+++ CheckDrive.Api/CheckDriver.Domain/DTOs/Driver/DriverForUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿using CheckDrive.Domain.DTOs.Account;
+
+namespace CheckDrive.Domain.DTOs.Driver
+{
+    public class DriverForUpdateDto
+    {
+        public AccountForUpdateDto AccountForUpdateDto { get; set; }
+    }
+}
