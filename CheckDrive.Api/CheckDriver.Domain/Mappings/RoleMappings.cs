7c59375aabc35f2505593aadeb4fd79de81fab0d
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-05-09 17:55:56 +0500

    Create-RoleMappings

---
+++ CheckDrive.Api/CheckDriver.Domain/Mappings/RoleMappings.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CheckDrive.Domain.DTOs.Role;
+using CheckDriver.Domain.Entities;
+
+namespace CheckDrive.Domain.Mappings
+{
+    public class RoleMappings : Profile
+    {
+        public RoleMappings() 
+        {
+            CreateMap<RoleDto, Role>();
+            CreateMap<Role, RoleDto>();
+            CreateMap<RoleForCreateDto, Role>();
+            CreateMap<RoleForUpdateDto, Role>();
+        }
+    }
+}
