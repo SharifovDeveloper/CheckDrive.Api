4fa558e31dc27925c597590b9e1ae653dd331898
diyorzz <128776299+diyorzz@users.noreply.github.com>
2024-05-09 13:50:01 +0500

    MechanicHandoverDTOs

---
+++ CheckDrive.Api/CheckDriver.Domain/DTOs/MechanicHandover/MechanicHandoverForUpdateDto.cs
@@ -0,0 +1,19 @@
+﻿using CheckDrive.Domain.DTOs.Car;
+using CheckDrive.Domain.DTOs.Driver;
+using CheckDrive.Domain.DTOs.Mechanic;
+using CheckDriver.Domain.Entities;
+
+namespace CheckDrive.Domain.DTOs.MechanicHandover
+{
+    public class MechanicHandoverForUpdateDto
+    {
+        public bool IsHanded { get; set; }
+        public string? Comments { get; set; }
+        public Status Status { get; set; }
+        public DateTime Date { get; set; }
+
+        public MechanicDto MechanicDto { get; set; }
+        public CarDto CarDto { get; set; }
+        public DriverDto DriverDto { get; set; }
+    }
+}
