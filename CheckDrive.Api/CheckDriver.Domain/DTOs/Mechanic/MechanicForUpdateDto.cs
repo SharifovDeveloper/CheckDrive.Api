e7fa061d2dd2a2a65fc5f4994fc6550599f1d74a
diyorzz <128776299+diyorzz@users.noreply.github.com>
2024-05-09 13:49:33 +0500

    MechanicDTOs

---
+++ CheckDrive.Api/CheckDriver.Domain/DTOs/Mechanic/MechanicForUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿using CheckDrive.Domain.DTOs.Account;
+
+namespace CheckDrive.Domain.DTOs.Mechanic
+{
+    public class MechanicForUpdateDto
+    {
+        public AccountForUpdateDto AccountForUpdateDto { get; set; }
+    }
+}
