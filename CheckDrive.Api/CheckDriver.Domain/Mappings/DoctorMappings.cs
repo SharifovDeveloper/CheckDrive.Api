7acde66a2933c88339111f2aa72d658698dc10d2
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-05-09 17:54:47 +0500

    Create-DoctorMappings

---
+++ CheckDrive.Api/CheckDriver.Domain/Mappings/DoctorMappings.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using CheckDrive.Domain.DTOs.Doctor;
+using CheckDriver.Domain.Entities;
+
+namespace CheckDrive.Domain.Mappings
+{
+    public class DoctorMappings : Profile
+    {
+        public DoctorMappings() 
+        {
+            CreateMap<DoctorDto, Doctor>();
+            CreateMap<Doctor, DoctorDto>();
+            CreateMap<DoctorForCreateDto, Doctor>();
+            CreateMap<DoctorForUpdateDto, Doctor>();
+        }
+    }
+}
