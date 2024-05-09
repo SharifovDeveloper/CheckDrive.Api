38cb3dca0871797ac3ae8484eba9276bc77582ff
diyorzz <128776299+diyorzz@users.noreply.github.com>
2024-05-09 13:50:14 +0500

    OperatorDTOs

---
+++ CheckDrive.Api/CheckDriver.Domain/DTOs/Operator/OperatorForUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿using CheckDrive.Domain.DTOs.Account;
+
+namespace CheckDrive.Domain.DTOs.Operator
+{
+    public class OperatorForUpdateDto
+    {
+        public AccountForUpdateDto AccountForUpdateDto { get; set; }
+    }
+}
