6677fea6ebaf9ff9e44926745337551916aaf193
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-05-09 13:14:27 +0500

    AccountResourceParameters

---
+++ CheckDrive.Api/CheckDriver.Domain/ResourceParameters/AccountResourceParameters.cs
@@ -0,0 +1,8 @@
+﻿namespace CheckDrive.Domain.ResourceParameters
+{
+    public class AccountResourceParameters : ResourceParametersBase
+    {
+        public int? RoleId { get; set; }
+        public override string OrderBy { get; set; } = "firstname";
+    }
+}
