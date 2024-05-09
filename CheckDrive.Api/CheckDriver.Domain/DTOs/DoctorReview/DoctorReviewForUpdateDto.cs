b0795909de3ee9c43a9ba0cdcd75ef562e394c5d
diyorzz <128776299+diyorzz@users.noreply.github.com>
2024-05-09 13:49:03 +0500

    DoctorReviewDTOs

---
+++ CheckDrive.Api/CheckDriver.Domain/DTOs/DoctorReview/DoctorReviewForUpdateDto.cs
@@ -0,0 +1,14 @@
+﻿using CheckDrive.Domain.DTOs.Doctor;
+using CheckDrive.Domain.DTOs.Driver;
+
+namespace CheckDrive.Domain.DTOs.DoctorReview
+{
+    public class DoctorReviewForUpdateDto
+    {
+        public bool IsHealthy { get; set; }
+        public string? Comments { get; set; }
+        public DateTime Date { get; set; }
+        public DriverDto DriverDto { get; set; }
+        public DoctorDto DoctorDto { get; set; }
+    }
+}
