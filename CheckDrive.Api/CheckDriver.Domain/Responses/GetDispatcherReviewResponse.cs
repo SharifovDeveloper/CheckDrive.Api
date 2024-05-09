488467f4fcada2f7eb4bf0a85effb5ff98cfa9a4
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-05-09 14:26:47 +0500

    create-GetDispatcherReviewResponse

---
+++ CheckDrive.Api/CheckDriver.Domain/Responses/GetDispatcherReviewResponse.cs
@@ -0,0 +1,8 @@
+﻿using CheckDrive.Domain.DTOs.DispatcherReview;
+
+namespace CheckDrive.Domain.Responses
+{
+    public class GetDispatcherReviewResponse : GetBaseResponse<DispatcherReviewDto>
+    {
+    }
+}
