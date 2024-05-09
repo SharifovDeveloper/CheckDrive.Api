4524b7b825fa07693a42594991dfee79fb7e0d65
diyorzz <128776299+diyorzz@users.noreply.github.com>
2024-05-09 15:20:01 +0500

    MechanicService interface

---
+++ CheckDrive.Api/CheckDriver.Domain/Interfaces/Services/IMechanicService.cs
@@ -0,0 +1,16 @@
+﻿using CheckDrive.Domain.DTOs.Mechanic;
+using CheckDrive.Domain.ResourceParameters;
+using CheckDrive.Domain.Responses;
+
+namespace CheckDrive.Domain.Interfaces.Services
+{
+    public interface IMechanicService
+    {
+        Task<IEnumerable<MechanicDto>> GetAllMechanicesAsync();
+        Task<GetBaseResponse<MechanicDto>> GetMechanicesAsync(MechanicResourceParameters resourceParameters);
+        Task<MechanicDto?> GetMechanicByIdAsync(int id);
+        Task<MechanicDto> CreateMechanicAsync(MechanicForCreateDto mechanicForCreate);
+        Task<MechanicDto> UpdateMechanicAsync(MechanicForUpdateDto mechanicForUpdate);
+        Task DeleteMechanicAsync(int id);
+    }
+}
