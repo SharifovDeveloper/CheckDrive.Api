57afbddc587e4ff43523cef1a0b71ff10fbc6e10
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-05-09 13:13:18 +0500

    MechanicAcceptanceResourceParameters

---
+++ CheckDrive.Api/CheckDriver.Domain/ResourceParameters/MechanicAcceptanceResourceParameters.cs
@@ -0,0 +1,15 @@
+﻿using CheckDriver.Domain.Entities;
+
+namespace CheckDrive.Domain.ResourceParameters
+{
+    public class MechanicAcceptanceResourceParameters : ResourceParametersBase
+    {
+        public bool? IsAccepted { get; set; }
+        public Status? Status { get; set; }
+        public DateTime? Date { get; set; }
+        public double? Distance { get; set; }
+        public double? DistanceLessThan { get; set; }
+        public double? DistanceGreaterThan { get; set; }
+        public override string OrderBy { get; set; } = "id";
+    }
+}
