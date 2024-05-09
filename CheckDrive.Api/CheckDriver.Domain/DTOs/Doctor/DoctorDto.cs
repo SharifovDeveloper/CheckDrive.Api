cd30f71338cb3fec5258ab037453307b652bfa5d
diyorzz <128776299+diyorzz@users.noreply.github.com>
2024-05-09 13:48:47 +0500

    DoctorDTOs

---
+++ CheckDrive.Api/CheckDriver.Domain/DTOs/Doctor/DoctorDto.cs
@@ -0,0 +1,10 @@
+﻿using CheckDrive.Domain.DTOs.Account;
+
+namespace CheckDrive.Domain.DTOs.Doctor
+{
+    public class DoctorDto
+    {
+        public int Id { get; set; }
+        public AccountDto AccountDto { get; set; }
+    }
+}
