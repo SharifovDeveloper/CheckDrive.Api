0ce7ff4bc1354bf60703dd4287fad25740376a00
diyorzz <128776299+diyorzz@users.noreply.github.com>
2024-05-09 15:19:50 +0500

    MechanicHandoverService interface

---
+++ CheckDrive.Api/CheckDriver.Domain/Interfaces/Services/IMechanicHandoverService.cs
@@ -0,0 +1,16 @@
+﻿using CheckDrive.Domain.DTOs.MechanicHandover;
+using CheckDrive.Domain.ResourceParameters;
+using CheckDrive.Domain.Responses;
+
+namespace CheckDrive.Domain.Interfaces.Services
+{
+    public interface IMechanicHandoverService
+    {
+        Task<IEnumerable<MechanicHandoverDto>> GetAllMechanicHandoversAsync();
+        Task<GetBaseResponse<MechanicHandoverDto>> GetMechanicHandoversAsync(MechanicHandoverResourceParameters resourceParameters);
+        Task<MechanicHandoverDto?> GetMechanicHandoverByIdAsync(int id);
+        Task<MechanicHandoverDto> CreateMechanicHandoverAsync(MechanicHandoverForCreateDto handoverForCreateDto);
+        Task<MechanicHandoverDto> UpdateMechanicHandoverAsync(MechanicHandoverForUpdateDto handoverForUpdateDto);
+        Task DeleteMechanicHandoverAsync(int id);
+    }
+}
