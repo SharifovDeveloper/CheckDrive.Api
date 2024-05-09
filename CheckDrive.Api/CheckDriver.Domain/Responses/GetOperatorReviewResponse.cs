0376ce2b66fae3f08a0432554af0113a868cd21a
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-05-09 14:30:57 +0500

    create-GetOperatorReviewResponse

---
+++ CheckDrive.Api/CheckDriver.Domain/Responses/GetOperatorReviewResponse.cs
@@ -0,0 +1,8 @@
+﻿using CheckDrive.Domain.DTOs.OperatorReview;
+
+namespace CheckDrive.Domain.Responses
+{
+    public class GetOperatorReviewResponse : GetBaseResponse<OperatorReviewDto>
+    {
+    }
+}
