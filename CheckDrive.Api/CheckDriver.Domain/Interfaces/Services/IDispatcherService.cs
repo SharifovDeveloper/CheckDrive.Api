1b34c08f80bbd771a1221c92e3c7ea6bf0d44518
diyorzz <128776299+diyorzz@users.noreply.github.com>
2024-05-09 15:18:44 +0500

    DispatcherService interface

---
+++ CheckDrive.Api/CheckDriver.Domain/Interfaces/Services/IDispatcherService.cs
@@ -0,0 +1,16 @@
+﻿using CheckDrive.Domain.DTOs.Dispatcher;
+using CheckDrive.Domain.ResourceParameters;
+using CheckDrive.Domain.Responses;
+
+namespace CheckDrive.Domain.Interfaces.Services
+{
+    public interface IDispatcherService
+    {
+        Task<IEnumerable<DispatcherDto>> GetAllDispatchersAsync();
+        Task<GetBaseResponse<DispatcherDto>> GetDispatchersAsync(DispatcherResourceParameters resourceParameters);
+        Task<DispatcherDto?> GetDispatcherByIdAsync(int id);
+        Task<DispatcherDto> CreateDispatcherAsync(DispatcherForCreateDto dispatcherForCreate);
+        Task<DispatcherDto> UpdateDispatcherAsync(DispatcherForUpdateDto dispatcherForUpdate);
+        Task DeleteDispatcherAsync(int id);
+    }
+}
