34cc0a75b0c32d2b01a9718e19ed259d428e88d1
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-05-11 13:25:36 +0500

    Create Doctor Controller and Service

--- CheckDrive.Api/CheckDriver.Domain/DTOs/Doctor/DoctorDto.cs
+++ CheckDrive.Api/CheckDriver.Domain/DTOs/Doctor/DoctorDto.cs
@@ -5,6 +5,7 @@ namespace CheckDrive.Domain.DTOs.Doctor
     public class DoctorDto
     {
         public int Id { get; set; }
-        public AccountDto AccountDto { get; set; }
+        public int AccountId { get; set; }
+        public AccountDto Account { get; set; }
     }
 }
--- CheckDrive.Api/CheckDriver.Domain/Interfaces/Services/IDoctorService.cs
+++ CheckDrive.Api/CheckDriver.Domain/Interfaces/Services/IDoctorService.cs
@@ -6,7 +6,6 @@ namespace CheckDrive.Domain.Interfaces.Services
 {
     public interface IDoctorService
     {
-        Task<IEnumerable<DoctorDto>> GetAllDoctorsAsync();
         Task<GetBaseResponse<DoctorDto>> GetDoctorsAsync(DoctorResourceParameters resourceParameters);
         Task<DoctorDto?> GetDoctorByIdAsync(int id);
         Task<DoctorDto> CreateDoctorAsync(DoctorForCreateDto doctorForCreate);
