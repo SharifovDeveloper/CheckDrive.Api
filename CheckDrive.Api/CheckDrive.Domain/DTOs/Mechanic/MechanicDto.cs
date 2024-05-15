dc45d30df601b4dcd7aed09b8330053760356a20
diyorzz <128776299+diyorzz@users.noreply.github.com>
2024-05-15 19:13:42 +0500

    MechanicDTOs update to record

--- CheckDrive.Api/CheckDrive.Domain/DTOs/Mechanic/MechanicDto.cs
+++ CheckDrive.Api/CheckDrive.Domain/DTOs/Mechanic/MechanicDto.cs
@@ -1,10 +1,13 @@
-﻿using CheckDrive.Domain.DTOs.Account;
-
-namespace CheckDrive.Domain.DTOs.Mechanic
+﻿namespace CheckDrive.Domain.DTOs.Mechanic;
+public record MechanicDto(
+    int Id,
+    string Login,
+    string Password,
+    string PhoneNumber,
+    string FirstName,
+    string LastName,
+    DateTime Bithdate)
 {
-    public class MechanicDto
-    {
-        public int Id { get; set; }
-        public int AccountId { get; set; }
-    }
+    // Parameterless constructor required by AutoMapper
+    public MechanicDto() : this(default, default, default, default, default, default, default) { }
 }
