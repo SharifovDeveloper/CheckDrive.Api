12ab1f21b975c5b767a32b826a52a6ec769c75ca
diyorzz <128776299+diyorzz@users.noreply.github.com>
2024-05-15 19:10:12 +0500

    CarDTOs update to record

--- CheckDrive.Api/CheckDrive.Domain/DTOs/Car/CarDto.cs
+++ CheckDrive.Api/CheckDrive.Domain/DTOs/Car/CarDto.cs
@@ -1,15 +1,9 @@
-﻿using CheckDrive.Domain.DTOs.MechanicHandover;
-
-namespace CheckDrive.Domain.DTOs.Car
-{
-    public class CarDto
-    {
-        public int Id { get; set; }
-        public string Model { get; set; }
-        public string Color { get; set; }
-        public string Number { get; set; }
-        public double MeduimFuelConsumption { get; set; }
-        public double FuelTankCapacity { get; set; }
-        public int ManufacturedYear { get; set; }
-    }
-}
+﻿namespace CheckDrive.Domain.DTOs.Car;
+public record CarDto(
+    int Id,
+    string Model,
+    string Color,
+    string Number,
+    double MeduimFuelConsumption,
+    double FuelTankCapacity,
+    int ManufacturedYear);
