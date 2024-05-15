f172a3c8160b25f30faaff355e0e4a7804fe8a2f
diyorzz <128776299+diyorzz@users.noreply.github.com>
2024-05-15 19:13:11 +0500

    DriverDTOs update to record

--- CheckDrive.Api/CheckDrive.Domain/DTOs/Driver/DriverDto.cs
+++ CheckDrive.Api/CheckDrive.Domain/DTOs/Driver/DriverDto.cs
@@ -1,10 +1,13 @@
-﻿using CheckDrive.Domain.DTOs.Account;
-
-namespace CheckDrive.Domain.DTOs.Driver
+﻿namespace CheckDrive.Domain.DTOs.Driver;
+public record DriverDto(
+    int Id,
+    string Login,
+    string Password,
+    string PhoneNumber,
+    string FirstName,
+    string LastName,
+    DateTime Bithdate)
 {
-    public class DriverDto
-    {
-        public int Id { get; set; }
-        public int AccountId { get; set; }
-    }
+    // Parameterless constructor required by AutoMapper
+    public DriverDto() : this(default, default, default, default, default, default, default) { }
 }
--- CheckDrive.Api/CheckDrive.Domain/DTOs/Driver/DriverForCreateDto.cs
+++ CheckDrive.Api/CheckDrive.Domain/DTOs/Driver/DriverForCreateDto.cs
@@ -1,9 +1,8 @@
-﻿using CheckDrive.Domain.DTOs.Account;
-
-namespace CheckDrive.Domain.DTOs.Driver
-{
-    public class DriverForCreateDto
-    {
-        public int AccountId { get; set; }
-    }
-}
+﻿namespace CheckDrive.Domain.DTOs.Driver;
+public record DriverForCreateDto(
+    string Login,
+    string Password,
+    string PhoneNumber,
+    string FirstName,
+    string LastName,
+    DateTime Birthdate);
