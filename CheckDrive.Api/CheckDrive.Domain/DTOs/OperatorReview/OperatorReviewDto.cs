e1261ce1f1f04fad0d00976ead5faab792271719
diyorzz <128776299+diyorzz@users.noreply.github.com>
2024-05-15 19:18:57 +0500

    OperatorReviewDTOs update to record

--- CheckDrive.Api/CheckDrive.Domain/DTOs/OperatorReview/OperatorReviewDto.cs
+++ CheckDrive.Api/CheckDrive.Domain/DTOs/OperatorReview/OperatorReviewDto.cs
@@ -1,18 +1,11 @@
-﻿using CheckDrive.Domain.DTOs.Driver;
-using CheckDrive.Domain.DTOs.Operator;
-using CheckDrive.Domain.Entities;
+﻿using CheckDrive.Domain.Entities;
 
-namespace CheckDrive.Domain.DTOs.OperatorReview
-{
-    public class OperatorReviewDto
-    {
-        public int Id { get; set; }
-        public double OilAmount { get; set; }
-        public string? Comments { get; set; }
-        public Status Status { get; set; }
-        public DateTime Date { get; set; }
-
-        public int OperatorId { get; set; }
-        public int DriverId { get; set; }
-    }
-}
+namespace CheckDrive.Domain.DTOs.OperatorReview;
+public record OperatorReviewDto(
+    int Id,
+    double OilAmount,
+    string? Comments,
+    Status Status,
+    DateTime Date,
+    int OperatorId,
+    int DriverId);
