0668a969f894b836d68f428a461b4080ab8089a1
diyorzz <128776299+diyorzz@users.noreply.github.com>
2024-05-15 19:14:35 +0500

    MechanicHandoverDTOs update to record

--- CheckDrive.Api/CheckDrive.Domain/DTOs/MechanicHandover/MechanicHandoverDto.cs
+++ CheckDrive.Api/CheckDrive.Domain/DTOs/MechanicHandover/MechanicHandoverDto.cs
@@ -1,21 +1,12 @@
-﻿using CheckDrive.Domain.DTOs.Car;
-using CheckDrive.Domain.DTOs.Driver;
-using CheckDrive.Domain.DTOs.Mechanic;
-using CheckDrive.Domain.DTOs.MechanicAcceptance;
-using CheckDrive.Domain.Entities;
+﻿using CheckDrive.Domain.Entities;
 
-namespace CheckDrive.Domain.DTOs.MechanicHandover
-{
-    public class MechanicHandoverDto
-    {
-        public int Id { get; set; }
-        public bool IsHanded { get; set; }
-        public string? Comments { get; set; }
-        public Status Status { get; set; }
-        public DateTime Date { get; set; }
-
-        public int MechanicId { get; set; }
-        public int CarId { get; set; }
-        public int DriverId { get; set; }
-    }
-}
+namespace CheckDrive.Domain.DTOs.MechanicHandover;
+public record MechanicHandoverDto(
+    int Id,
+    bool IsHanded,
+    string? Comments,
+    Status Status,
+    DateTime Date,
+    int MechanicId,
+    int CarId,
+    int DriverId);
--- CheckDrive.Api/CheckDrive.Domain/DTOs/MechanicHandover/MechanicHandoverForUpdateDto.cs
+++ CheckDrive.Api/CheckDrive.Domain/DTOs/MechanicHandover/MechanicHandoverForUpdateDto.cs
@@ -1,17 +1,12 @@
 ﻿using CheckDrive.Domain.Entities;
 
-namespace CheckDrive.Domain.DTOs.MechanicHandover
-{
-    public class MechanicHandoverForUpdateDto
-    {
-        public int Id { get; set; }
-        public bool IsHanded { get; set; }
-        public string? Comments { get; set; }
-        public Status Status { get; set; }
-        public DateTime Date { get; set; }
-
-        public int MechanicId { get; set; }
-        public int CarId { get; set; }
-        public int DriverId { get; set; }
-    }
-}
+namespace CheckDrive.Domain.DTOs.MechanicHandover;
+public record MechanicHandoverForUpdateDto(
+    int Id,
+    bool IsHanded,
+    string? Comments,
+    Status Status,
+    DateTime Date,
+    int MechanicId,
+    int CarId,
+    int DriverId);
