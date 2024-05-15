c2720834200239a461c8011e43f9299dc0d5f735
diyorzz <128776299+diyorzz@users.noreply.github.com>
2024-05-15 19:11:41 +0500

    AccountDTOs update to record

--- CheckDrive.Api/CheckDrive.Domain/DTOs/Account/AccountForCreateDto.cs
+++ CheckDrive.Api/CheckDrive.Domain/DTOs/Account/AccountForCreateDto.cs
@@ -1,16 +1,9 @@
-﻿using CheckDrive.Domain.DTOs.Role;
-
-namespace CheckDrive.Domain.DTOs.Account
-{
-    public class AccountForCreateDto
-    {
-        public string Login { get; set; }
-        public string Password { get; set; }
-        public string PhoneNumber { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateTime Bithdate { get; set; }
-
-        public int RoleId { get; set; }
-    }
-}
+﻿namespace CheckDrive.Domain.DTOs.Account;
+public record AccountForCreateDto(
+    string Login,
+    string Password,
+    string PhoneNumber,
+    string FirstName,
+    string LastName,
+    DateTime Bithdate,
+    int RoleId);
