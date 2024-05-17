dfc81f449fd8d85521c594c28f1ad32daa22b339
SharifovDeveloper <sharifovmuhammadali571@gmail.com>
2024-05-17 18:34:22 +0500

    Removed DTOs file

--- CheckDrive.Api/CheckDrive.Domain/DTOs/Dispatcher/DispatcherForCreateDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CheckDrive.Domain.DTOs.Dispatcher;
-public record DispatcherForCreateDto(
-    string Login,
-    string Password,
-    string PhoneNumber,
-    string FirstName,
-    string LastName,
-    DateTime Birthdate);
--- CheckDrive.Api/CheckDrive.Domain/DTOs/DispatcherReview/DispatcherReviewForCreateDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CheckDrive.Domain.DTOs.DispatcherReview;
-public record DispatcherReviewForCreateDto(
-    double FuelSpended,
-    double DistanceCovered,
-    DateTime Date,
-    int DispatcherId,
-    int OperatorId,
-    int MechanicId,
-    int DriverId);
