8ff745d91f34715e842dc1c9d8489e789b5fe571
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2024-05-29 20:36:09 +0500

    Create JwtOption for token generate

---
+++ CheckDrive.Api/CheckDrive.Infrastructure/JwtToken/JwtOptions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckDrive.Infrastructure.JwtToken
+{
+    public class JwtOptions
+    {
+        public string SecretKey { get; set; } = string.Empty;
+        public int ExpiresHours { get; set; }
+    }
+}
