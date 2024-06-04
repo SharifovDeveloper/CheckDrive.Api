836cdcc92d4b92180221148b37101c60914a04e8
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2024-06-04 22:54:11 +0500

    Create ChatHub Service and inteface for send request and response

---
+++ CheckDrive.Api/CheckDrive.Domain/Interfaces/Hubs/IChatHub.cs
@@ -0,0 +1,9 @@
+﻿namespace CheckDrive.Domain.Interfaces.Hubs
+{
+    public interface IChatHub
+    {
+        Task SendPrivateRequest(string userId, string massege);
+
+        Task SendPrivateResponse(string employeeId, bool isApproved);
+    }
+}
---
+++ CheckDrive.Api/CheckDrive.Services/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿using CheckDrive.Domain.Interfaces.Hubs;
+using Microsoft.AspNetCore.SignalR;
+
+namespace CheckDrive.Services.Hubs
+{
+    public class ChatHub : Hub, IChatHub
+    {
+        public async Task SendPrivateRequest(string userId, string massege)
+        {
+            await Clients.User(userId).SendAsync("ReceiveRequest", massege);
+        }
+
+        public async Task SendPrivateResponse(string employeeId, bool isApproved)
+        {
+            await Clients.User(employeeId).SendAsync("ReceiveResponse", isApproved);
+        }
+    }
+}
