e45383485ebeccdb98417c716fdb8be20fc04953
Sharafiddin.Mukhtorov <129146459+SharafiddinMukhtorov@users.noreply.github.com>
2024-06-10 16:35:23 +0500

    Updated DispatcherReview Entity and Mappings

--- CheckDrive.Api/CheckDrive.Domain/Entities/DispatcherReview.cs
+++ CheckDrive.Api/CheckDrive.Domain/Entities/DispatcherReview.cs
@@ -8,6 +8,8 @@ namespace CheckDrive.Domain.Entities
         public DateTime Date { get; set; }
         public int DispatcherId { get; set; }
         public Dispatcher Dispatcher { get; set; }
+        public int CarId { get; set; }
+        public Car? Car { get; set; }
         public int OperatorId { get; set; }
         public Operator Operator { get; set; }
         public int MechanicId { get; set; }
--- CheckDrive.Api/CheckDrive.Domain/Mappings/DispatcherReviewMappings.cs
+++ CheckDrive.Api/CheckDrive.Domain/Mappings/DispatcherReviewMappings.cs
@@ -10,6 +10,7 @@ namespace CheckDrive.Domain.Mappings
         {
             CreateMap<DispatcherReviewDto, DispatcherReview>();
             CreateMap<DispatcherReview, DispatcherReviewDto>()
+                .ForMember(d => d.CarName, f => f.MapFrom(e => e.Car.Model))
                 .ForMember(d => d.DriverName, f => f.MapFrom(e => $"{e.Driver.Account.FirstName} {e.Driver.Account.LastName}"))
                 .ForMember(d => d.DispatcherName, f => f.MapFrom(e => $"{e.Dispatcher.Account.FirstName} {e.Dispatcher.Account.LastName}"))
                 .ForMember(d => d.MechanicName, f => f.MapFrom(e => $"{e.Mechanic.Account.FirstName} {e.Mechanic.Account.LastName}"))
