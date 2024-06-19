8ad0b626728707fd90692e9f8ef425acf3f22ba6
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2024-06-19 12:27:09 +0500

    Add AccountDriverId mapping

--- CheckDrive.Api/CheckDrive.Domain/Mappings/MechanicHandoverMappings.cs
+++ CheckDrive.Api/CheckDrive.Domain/Mappings/MechanicHandoverMappings.cs
@@ -12,7 +12,8 @@ namespace CheckDrive.Domain.Mappings
             CreateMap<MechanicHandover, MechanicHandoverDto>()
                 .ForMember(d => d.CarName, f => f.MapFrom(e => e.Car.Model))
                 .ForMember(d => d.DriverName, f => f.MapFrom(e => $"{e.Driver.Account.FirstName} {e.Driver.Account.LastName}"))
-                .ForMember(d => d.MechanicName, f => f.MapFrom(e => $"{e.Mechanic.Account.FirstName} {e.Mechanic.Account.LastName}"));
+                .ForMember(d => d.MechanicName, f => f.MapFrom(e => $"{e.Mechanic.Account.FirstName} {e.Mechanic.Account.LastName}"))
+                .ForMember(x => x.AccountDriverId, f => f.MapFrom(e => e.Driver.Account.Id));
             CreateMap<MechanicHandoverForCreateDto, MechanicHandover>();
             CreateMap<MechanicHandoverForUpdateDto, MechanicHandover>();
         }
