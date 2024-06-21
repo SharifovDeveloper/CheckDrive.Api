ba1e2272b66628f40be07ce07f5dfc88f6a25a35
AzamatG17 <128783946+AzamatG17@users.noreply.github.com>
2024-06-21 12:10:23 +0500

    Delete PassswordHacher class and intefase and packege

--- CheckDrive.Api/CheckDrive.Domain/Interfaces/Auth/IPasswordHasher.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CheckDrive.Domain.Interfaces.Auth
-{
-    public interface IPasswordHasher
-    {
-        string Generate(string password);
-        bool Verify(string password, string hashedPassword);
-    }
-}
