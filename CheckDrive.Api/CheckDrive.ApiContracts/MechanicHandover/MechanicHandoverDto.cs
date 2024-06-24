6df1297d15d0605becf5a1b74e00b8bb28355108
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-06-24 16:32:40 +0500

    updated

--- CheckDrive.Api/CheckDrive.ApiContracts/MechanicHandover/MechanicHandoverDto.cs
+++ CheckDrive.Api/CheckDrive.ApiContracts/MechanicHandover/MechanicHandoverDto.cs
@@ -5,7 +5,7 @@ namespace CheckDrive.ApiContracts.MechanicHandover
     public class MechanicHandoverDto
     {
         public int Id { get; set; }
-        public bool IsHanded { get; set; }
+        public bool? IsHanded { get; set; }
         public string Comments { get; set; } = "";
         public StatusForDto Status { get; set; }
         public DateTime Date { get; set; }
