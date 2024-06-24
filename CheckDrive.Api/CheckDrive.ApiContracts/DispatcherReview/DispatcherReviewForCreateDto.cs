ea80dbad8610c7c8bb4ae451a631eec7112c5450
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-06-24 15:28:26 +0500

    Api_contract

--- CheckDrive.Api/CheckDrive.ApiContracts/DispatcherReview/DispatcherReviewForCreateDto.cs
+++ CheckDrive.Api/CheckDrive.ApiContracts/DispatcherReview/DispatcherReviewForCreateDto.cs
@@ -11,5 +11,8 @@ namespace CheckDrive.ApiContracts.DispatcherReview
         public int OperatorId { get; set; }
         public int MechanicId { get; set; }
         public int DriverId { get; set; }
+        public int CarId { get; set; }
+        public int MechanicAcceptanceId { get; set; }
+        public int MechanicHandoverId { get; set; }
     }
 }
