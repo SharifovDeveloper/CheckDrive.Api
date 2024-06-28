fc02c933992a2f91aec4b60b58b18e27295f1b76
Diyor Akhmatov <128776299+diyorzz@users.noreply.github.com>
2024-06-28 11:40:49 +0500

    Add validation to Car

--- CheckDrive.Api/CheckDrive.ApiContracts/Car/CarForCreateDto.cs
+++ CheckDrive.Api/CheckDrive.ApiContracts/Car/CarForCreateDto.cs
@@ -1,13 +1,31 @@
-﻿namespace CheckDrive.ApiContracts.Car
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CheckDrive.ApiContracts.Car
 {
     public class CarForCreateDto
     {
+        [Required(ErrorMessage = "Modelni kiritish majburiy")]
         public string Model { get; set; }
+
+        [Required(ErrorMessage = "Rangni kiritish majburiy")]
         public string Color { get; set; }
+
+        [Required(ErrorMessage = "Raqamni kiritish majburiy")]
         public string Number { get; set; }
+
+        [Required(ErrorMessage = "O'rtacha yoqilg'i sarfini kiritish majburiy")]
+        [Range(0, double.MaxValue, ErrorMessage = "O'rtacha yoqilg'i sarfi manfiy bo'lishi mumkin emas")]
         public double MeduimFuelConsumption { get; set; }
+
+        [Required(ErrorMessage = "Yoqilg'i hajmini kiritish majburiy")]
+        [Range(0, double.MaxValue, ErrorMessage = "Yoqilg'i hajmi manfiy bo'lishi mumkin emas")]
         public double RemainingFuel { get; set; }
+
+        [Required(ErrorMessage = "Yoqilg'i baki sig'imini kiritish majburiy")]
+        [Range(0, double.MaxValue, ErrorMessage = "Yoqilg'i baki sig'imi manfiy bo'lishi mumkin emas")]
         public double FuelTankCapacity { get; set; }
+
+        [Required(ErrorMessage = "Ishlab chiqarilgan yilni kiritish majburiy")]
         public int ManufacturedYear { get; set; }
     }
 }
