c51f7a6b30e729fadb02b0b0f6927e07b7a2d3db
khojianvar <khojianvarabdumukhtorov@gmail.com>
2024-07-08 14:46:15 +0500

    add-status-column

--- CheckDrive.Api/CheckDrive.ApiContracts/OperatorReview/OperatorReviewForUpdateDto.cs
+++ CheckDrive.Api/CheckDrive.ApiContracts/OperatorReview/OperatorReviewForUpdateDto.cs
@@ -9,6 +9,7 @@ namespace CheckDrive.ApiContracts.OperatorReview
 
         [Range(0, double.MaxValue, ErrorMessage = "Quyiladigan yoqilg'i manfiy bo'lishi mumkin emas")]
         public double OilAmount { get; set; }
+        public bool? IsGiven { get; set; }
         public string Comments { get; set; } = "";
         public StatusForDto Status { get; set; }
         public OilMarksForDto OilMarks { get; set; }
