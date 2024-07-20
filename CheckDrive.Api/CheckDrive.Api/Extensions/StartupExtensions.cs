c11a2d78983ab720f820fef012293cc90a4d5c32
Miraziz Khidoyatov <khidoyatov1997@gmail.com>
2024-07-20 11:13:08 -0700

    update application startup

---
+++ CheckDrive.Api/CheckDrive.Api/Extensions/StartupExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace CheckDrive.Api.Extensions;
+
+public static class StartupExtensions
+{
+    public static IApplicationBuilder UseErrorHandler(this IApplicationBuilder app)
+    {
+        app.UseMiddleware<ExceptionHandlerMiddleware>();
+
+        return app;
+    }
+}
